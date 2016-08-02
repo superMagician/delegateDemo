472c3f439e9b7520916b1f2e63f420c6389336ed
姚哥 <yaoge.com>
2016-08-02 20:29:34 +0800

    项目准备

---
+++ delegataDemo/delegataDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  delegataDemo
+//
+//  Created by 大姚 on 16/8/2.
+//  Copyright © 2016年 王姚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ delegataDemo/delegataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delegataDemo
+//
+//  Created by 大姚 on 16/8/2.
+//  Copyright © 2016年 王姚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
